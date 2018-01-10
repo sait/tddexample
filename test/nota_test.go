03dd3cd6d889ab5928c163bae17691deca6aff5c
xerardoo <gerardoo_2413@hotmail.com>
2018-01-09 17:53:38 -0700

    se implementaron las pruebas con el modelo de nota

--- test/nota_test.go
+++ test/nota_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"NotaFactura/models"
 )
 
 func TestNota(t *testing.T) {
@@ -14,30 +15,93 @@ func TestNota(t *testing.T) {
 
 var _ = Describe("Una nota de compra", func() {
 
+	//se crea una nueva nota
+	nota := &models.Nota{}
+
 	Context("Inicialmente Tiene ", func() {
-		It("cliente vacio", func() {})
-		It("0 partidas", func() {})
-		Specify("el total es de 0.00", func() {})
+
+		It("cliente vacio", func() {
+			Expect(nota.GetCliente()).Should(BeEmpty())
+		})
+
+		It("0 partidas", func() {
+			Expect(nota.CountPartidas()).Should(BeZero())
+		})
+
+		Specify("el total es de 0.00", func() {
+			Expect(nota.GetTotal()).Should(BeZero())
+		})
 	})
 
 	Context("Cuando se define el cliente", func() {
-		It("la nota tiene definido el valor dado", func() {})
+
+		//se define el valor del cliente
+		nota.SetCliente("Jose Perez Leon")
+
+		It("la nota tiene definido el valor dado", func() {
+			Expect(nota.Cliente).Should(Equal("Jose Perez Leon"))
+		})
 	})
 
 	Context("Cuando se agrega un articulo", func() {
-		It("la nota tiene 1 partida mas", func() {})
+
+		//se obtiene total antes de agregar
+		totalnota := nota.GetTotal()
+
+		//se agrega una partida a la nota
+		partida1 := models.Partida{Id: 1, Descripcion: "ABRECUBETAS MAX", Cantidad: 3, Precio: 40.50}
+		nota.AddPartida(partida1)
+
+		It("la nota tiene 1 partida mas", func() {
+			Expect(nota.CountPartidas()).Should(Equal(nota.CountPartidas() + 1))
+		})
+
 		It("el importe de la partida se calcula", func() {})
-		Specify("se acumula el importe de la factura al total de la nota", func() {})
+		Specify("se acumula el importe de la factura al total de la nota", func() {
+			Expect(nota.GetTotal()).Should(Equal(totalnota + partida1.GetImporte()))
+		})
 	})
 
-	Context("Cuando se modifica la partida", func() {
-		It("si la cantidad es cero, la partida se elimina", func() {})
-		It("si la cantidad se modifica y es mayor a cero, se re calcula el importe", func() {})
-		Specify("si el importe cambia, se actualiza el total de la nota", func() {})
+	Context("Cuando se modifica la cantidad de la partida", func() {
+		//se  obtiene el total antes de actualizar
+		totalnota := nota.GetTotal()
+
+		partida2 := nota.GetPartida(1) //se obtiene partida a modificar
+		partida2.Cantidad = 6
+		nota.UpdPartida(partida2)
+
+		//se calcula importe que debe de dar
+		importe := partida2.Cantidad * partida2.Precio
+
+		It("se re calcula el importe", func() {
+			Expect(partida2.Importe).Should(Equal(importe))
+		})
+
+		Specify("se actualiza el total de la nota", func() {
+			Expect(nota.GetTotal()).Should(Equal(totalnota + importe))
+		})
+
+		Context("si es 0 cero", func() {
+
+			partida2 := nota.GetPartida(1) //se obtiene partida a modificar
+			partida2.Cantidad = 0
+			nota.UpdPartida(partida2)
+
+			It("la partida se elimina", func() {
+				Expect(nota.Exist(partida2.Id)).Should(BeFalse())
+			})
+		})
 	})
 
 	Context("Cuando se elimina una partida", func() {
-		It("la nota debe de tener una partida menos", func() {})
+
+		totalpartidas := nota.CountPartidas()
+		nota.DltPartida(1)
+
+		It("la nota debe de tener una partida menos", func() {
+			Expect(nota.CountPartidas()).Should(Equal(totalpartidas - 1))
+		})
+
 		Specify("el total de la nota se actualiza", func() {})
 	})
 
