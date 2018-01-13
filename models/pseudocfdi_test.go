79a67782161c4d9653d7f3f354d9a951b475924d
xerardoo <gerardoo_2413@hotmail.com>
2018-01-13 14:41:15 -0700

    actuaizacion de pruebas ok

--- models/pseudocfdi_test.go
+++ models/pseudocfdi_test.go
@@ -2,11 +2,8 @@ package models
 
 import (
 	"testing"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/sait/tddexample/models"
-	"fmt"
 )
 
 func TestPsuedoCFDI(t *testing.T) {
@@ -19,13 +16,10 @@ var _ = Describe("Un cfdi tipo ingreso", func() {
 	//se crea una nota inicial
 	nota := new(Nota)
 	nota.SetCliente("Epifanio de la Torre")
-	partida1 := &Partida{Id: 1, Descripcion: "MATABICHOS 400ml", Cantidad: 1, Precio: 25}
+	partida1 := &Partida{Id: 1, Descripcion: "MATABICHOS 400ML", Cantidad: 1, Precio: 25}
 	partida2 := &Partida{Id: 2, Descripcion: "TALLADOR TURBO", Cantidad: 50, Precio: 10}
 	nota.AddPartida(partida1)
 	nota.AddPartida(partida2)
-
-	_,data:=nota.ToXML()
-	fmt.Println(data)
 
 	Context("Para poder facturar debe de tener por lo menos ", func() {
 		It("cliente", func() {
@@ -43,31 +37,54 @@ var _ = Describe("Un cfdi tipo ingreso", func() {
 
 	Context("En la estructura del CFDI XML debe existir", func() {
 
+		cfdi := nota.ToCFDI()
+
 		Context("un nodo Comprobante", func() {
-			It("debe de tener un attributo version con el valor 3.3", func() {})
+			It("debe de tener un attributo version con el valor 3.3", func() {
+
+				Expect(cfdi.Version).Should(Equal("3.3"))
+			})
 			It(" debe de tener un attributo total", func() {})
-			Specify(" debe de tener el total de 525", func() {})
+			Specify(" debe de tener el total de 525", func() {
+				Expect(cfdi.Total).Should(Equal("525.00"))
+			})
 		})
 
-		Context("un nodo Receptor con el nombre del cliente", func() {})
+		Context("un nodo Receptor con el nombre del cliente", func() {
+			It(" con el valor", func() {
+				Expect(cfdi.Receptor.Nombre).To(Equal("Epifanio de la Torre"))
+			})
+		})
 
 		Context("nodos Concepto", func() {
-			Specify("deben de ser 2 nodos", func() {})
 
-			Specify("el nodo con indice 0", func() {
-				It("la descripcion debe de ser", func() {})
-				It("la cantidad debe de ser", func() {})
-				It("el precio debe de ser", func() {})
+			Specify("deben de ser 2 nodos", func() {
+				Expect(len(cfdi.Conceptos.Concepto)).Should(Equal(2))
 			})
 
-			Specify("el nodo con indice 1", func() {
-				It("la descripcion debe de ser", func() {})
-				It("la cantidad debe de ser", func() {})
-				It("el precio debe de ser", func() {})
+			Context("el nodo con indice 0", func() {
+				It("la descripcion debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[0].Descripcion).Should(Equal("MATABICHOS 400ML"))
+				})
+				It("la cantidad debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[0].Cantidad).Should(Equal("1.00"))
+				})
+				It("el precio debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[0].ValorUnitario).Should(Equal("25.00"))
+				})
 			})
 
-			_,data:=nota.ToXML()
-			fmt.Println(data)
+			Context("el nodo con indice 1", func() {
+				It("la descripcion debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[1].Descripcion).Should(Equal("TALLADOR TURBO"))
+				})
+				It("la cantidad debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[1].Cantidad).Should(Equal("50.00"))
+				})
+				It("el precio debe de ser", func() {
+					Expect(cfdi.Conceptos.Concepto[1].ValorUnitario).Should(Equal("10.00"))
+				})
+			})
 		})
 	})
 })
