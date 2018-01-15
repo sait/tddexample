7e40acd402ae3a8862f9ffa246237b167f5eebbf
xerardoo <gerardoo_2413@hotmail.com>
2018-01-15 16:11:17 -0700

    se documentan metodos

--- models/nota.go
+++ models/nota.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 )
 
+//Estructura Raiz de Nota
 type Nota struct {
 	Cliente  string
 	Partidas []*Partida //arreglo de apuntadores
 	Total    float64
 }
 
+//Estructura para representar una partida
 type Partida struct {
 	Id          int
 	Descripcion string
@@ -18,14 +20,17 @@ type Partida struct {
 	Importe     float64
 }
 
+//Metodo para definir valor del atributo cliente
 func (n *Nota) SetCliente(cliente string) {
 	n.Cliente = cliente
 }
 
+//Metodo para obtener el valor del atributo cliente
 func (n *Nota) GetCliente() string {
 	return n.Cliente
 }
 
+//Metodo para obtener el valor de una partida por Id
 func (n *Nota) GetPartida(id int) *Partida {
 	finded := &Partida{}
 	for _, pa := range n.Partidas {
@@ -36,12 +41,14 @@ func (n *Nota) GetPartida(id int) *Partida {
 	return finded
 }
 
+//Metodo para agregar una partida a una nota
 func (n *Nota) AddPartida(p *Partida) {
 	p.Calcular()
 	n.Partidas = append(n.Partidas, p)
 	n.Calcular()
 }
 
+//Metodo para actualizar los valores de una partida
 func (n *Nota) UpdPartida(p *Partida) {
 	if p.Cantidad == 0 {
 		n.DltPartida(p.Id)
@@ -55,6 +62,7 @@ func (n *Nota) UpdPartida(p *Partida) {
 	n.Calcular()
 }
 
+//Metodo para eliminar una partida
 func (n *Nota) DltPartida(id int) {
 	for i, pa := range n.Partidas {
 		if id == pa.Id {
@@ -66,10 +74,12 @@ func (n *Nota) DltPartida(id int) {
 	n.Calcular()
 }
 
+//Metodo para obtener el numero de partidas de una nota
 func (n *Nota) CountPartidas() int {
 	return len(n.Partidas)
 }
 
+//Metodo para saber si existe una partida en una nota
 func (n *Nota) Exist(id int) bool {
 	finded := false
 	for _, pa := range n.Partidas {
@@ -80,6 +90,7 @@ func (n *Nota) Exist(id int) bool {
 	return finded
 }
 
+//Metodo para calcular los montos de una partida
 func (n *Nota) Calcular() float64 {
 	n.Total = 0
 	for _, p := range n.Partidas {
@@ -89,13 +100,17 @@ func (n *Nota) Calcular() float64 {
 	return n.Total
 }
 
+//Metodo para obtener el monto total de una nota
 func (n *Nota) GetTotal() float64 {
 	return n.Total
 }
 
+//Metodo para obtener el importe de una partida
 func (p *Partida) GetImporte() float64 {
 	return p.Importe
 }
+
+//Metodo para calcular los montos de una partida
 func (p *Partida) Calcular() float64 {
 	p.Importe = 0
 	p.Importe = p.Cantidad * p.Precio
@@ -103,6 +118,7 @@ func (p *Partida) Calcular() float64 {
 	return p.Importe
 }
 
+//Metodo para convertir una nota a formato JSON
 func (n *Nota) ToJSON() (error, string) {
 	b, err := json.Marshal(n)
 	if err != nil {
--- models/pseudocfdi.go
+++ models/pseudocfdi.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 )
 
+//Estructura raiz de un cfdi
 type CFDI struct {
 	XMLName     xml.Name    `xml:" Comprobante,omitempty"`
 	Version     string      `xml:" Version,attr,omitempty"`
@@ -17,15 +18,18 @@ type CFDI struct {
 	Complemento Complemento `xml:" Complemento,omitempty"`
 }
 
+//Estructura para representar el receptor
 type Receptor struct {
 	Nombre string `xml:" Nombre,attr,omitempty"`
 }
 
+//Estructura para representar los conceptos
 type Conceptos struct {
 	Concepto []*Concepto `xml:" Concepto,omitempty"`
 	XMLName  xml.Name    `xml:" Conceptos,omitempty"`
 }
 
+//Estructura para representar un concepto
 type Concepto struct {
 	XMLName          xml.Name `xml:" Concepto,omitempty"`
 	NoIdentificacion string   `xml:" NoIdentificacion,attr,omitempty"`
@@ -35,11 +39,13 @@ type Concepto struct {
 	Importe          string   `xml:" Importe,attr,omitempty"`
 }
 
+//Estructura para representar el complemento
 type Complemento struct {
 	TimbreFiscalDigital TimbreFiscalDigital `xml:" TimbreFiscalDigital,omitempty"`
 	XMLName             xml.Name            `xml:" Complemento,omitempty"`
 }
 
+//Estructura para representar el timbre
 type TimbreFiscalDigital struct {
 	FechaTimbrado    string   `xml:" FechaTimbrado,attr, omitempty"`
 	NoCertificadoSAT string   `xml:" NoCertificadoSAT,attr,omitempty"`
@@ -50,6 +56,7 @@ type TimbreFiscalDigital struct {
 	XMLName          xml.Name `xml:" TimbreFiscalDigital, omitempty"`
 }
 
+//Metodo para convertir una nota a cfdi
 func (n *Nota) ToCFDI() *CFDI {
 
 	cfdi := &CFDI{}
@@ -72,6 +79,7 @@ func (n *Nota) ToCFDI() *CFDI {
 	return cfdi
 }
 
+//Metodo para convertir un cfdi a xml
 func (c *CFDI) ToXML() (string, error) {
 	b, err := xml.MarshalIndent(&c, "  ", "   ")
 	if err != nil {
@@ -80,6 +88,7 @@ func (c *CFDI) ToXML() (string, error) {
 	return string(b), err
 }
 
+//Metodo para timbrar un cfdi
 func (c *CFDI) Timbrar() {
 	//mock timbrado
 	timbre := TimbreFiscalDigital{}
@@ -97,6 +106,7 @@ func (c *CFDI) Timbrar() {
 	c.ToFile(b, "nota.xml")
 }
 
+//Metodo para guardar un cfdi en un archivo xml
 func (c *CFDI) ToFile(sxml string, filename string) {
 	//dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	//if err != nil {
@@ -109,6 +119,7 @@ func (c *CFDI) ToFile(sxml string, filename string) {
 	}
 }
 
+//Metodo para convertir un float64 a string
 func floattostr(fv float64) string {
 	return strconv.FormatFloat(fv, 'f', 2, 64)
 }
